58fbe7c1ef274d548686b7d377ab1db704a3fcea
nandodoria <nandodoria@hotmail.ca>
2019-03-27 13:22:13 -0400

    making chat functional

---
+++ public/js/main_vm.js
@@ -0,0 +1,42 @@
+import ChatMessage from './modules/ChatMessage.js';
+
+const socket = io();
+
+function logConnect({sID, message}) {
+    console.log(sID, message);
+    vm.socketID = sID;
+}
+
+function appendMessage(message) {
+    vm.messages.push(message);
+}
+
+//create vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        nickname: "",
+        message: "",
+        messages: []
+    },
+
+    methods: {
+        dispatchMessage(){
+            //emit message event from the client side
+            socket.emit('chat message', {content: this.message, name: this.nickname || "Anonymous"});
+
+            //reset the message field
+            this.message = "";
+        }
+    },
+
+
+    components: {
+        newmessage: ChatMessage
+    }
+
+}).$mount(`#app`);
+
+socket.on('connected', logConnect);
+socket.addEventListener('chat message', appendMessage);
+socket.addEventListener('disconnect', appendMessage);
